874f9fa9c08ecdb974bf4f88edc7e4b5a3d76ddc
su-san-mt97 <48282851+su-san-mt97@users.noreply.github.com>
2022-02-09 22:18:18 +0900

    商品詳細ページの動的対応

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,21 +2,45 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'This area is sample text 1',
+    headingtext2: 'This area is sample text 2',
+    headingtext3: 'This area is sample text 3'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'This area is sample text 1',
+    headingtext2: 'This area is sample text 2',
+    headingtext3: 'This area is sample text 3'
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'A normal phone with one of the best cameras',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'This area is sample text 1',
+    headingtext2: 'This area is sample text 2',
+    headingtext3: 'This area is sample text 3'
   },
   {
     name: 'Phone Pro',
     price: 1000,
-    description: 'A Profesional phone with one of the marberas screens'
+    description: 'A Profesional phone with one of the marberas screens',
+    heading1: 'Heading1',
+    heading2: 'Heading2',
+    heading3: 'Heading3',
+    headingtext1: 'This area is sample text 1',
+    headingtext2: 'This area is sample text 2',
+    headingtext3: 'This area is sample text 3'
   }
 ];
