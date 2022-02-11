8f6b53e8387feae74c026c7b3f8d27d953242320
su-san-mt97 <48282851+su-san-mt97@users.noreply.github.com>
2022-02-11 14:45:22 +0900

    Nodeサーバー構築

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const config = require('./config/dev')
+const FakeDb = require('./fake-db')
+
+const productRoutes = require('./routes/products')
+
+mongoose.connect(config.DB_URI).then(() => {
+    const fakeDb = new FakeDb()
+    fakeDb.initDb();
+})
 
 const app = express()
-app.get('/products', function(req, res) {
-    res.json({'success': true})
-})
+
+app.use('/api/v1/products', productRoutes)
 
 const PORT = process.env.PORT || '3001'
 
@@ -13,5 +20,4 @@ app.listen(PORT, function() {
     console.log('I am running!')
 })
 
-mongoose.connect(config.DB_URI);
 
